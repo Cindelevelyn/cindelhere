2ac8c03c53451bc584f23e9a76d545908697f565
Cindel Sousa <cindelevelyn@gmail.com>
2022-08-01 22:07:26 -0300

    animation page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,26 +7,29 @@ import { Projects } from "./Projects";
 
 export function Index() {
   return (
-    <div id="home" className="bg-bege flex flex-col">
-      <div className="bg-bege sticky top-0">
-        <Header
-          title="&lsaquo;home"
-          title2="&lsaquo;about&rsaquo;"
-          title3="&lsaquo;projects&rsaquo;"
-          title4="&lsaquo;experience&rsaquo;"
-          flag="1"
-        />
-      </div>
-      <div>
-        <Home />
-        <div id="about">
-          <About />
+    <div className="h-[100vh] snap-y snap-mandatory overflow-y-scroll">
+      <div id="home" className="bg-bege flex flex-col snap-start">
+        <div className="bg-bege sticky top-0">
+          <Header
+            title="&lsaquo;home"
+            title2="&lsaquo;about&rsaquo;"
+            title3="&lsaquo;projects&rsaquo;"
+            title4="&lsaquo;experience&rsaquo;"
+            flag="1"
+          />
         </div>
-        <div id="projects">
-          <Projects />
-        </div>
-        <div className="flex right-0 h-full fixed mt-60">
-          <Social />
+        <div className="snap-start">
+          <Home />
+          <div id="about" className="snap-start">
+            <About />
+          </div>
+          <div id="projects" className="snap-start">
+            <Projects />
+          </div>
+
+          <div className="flex right-0 h-full fixed mt-60">
+            <Social />
+          </div>
         </div>
       </div>
       <footer className="bg-bege text-gray opacity-30 text-center">
